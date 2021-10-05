ac412e4361c4d9a58213160ae270158fd83f692d
ronaldoapf <ronaldo.alves.1997@gmail.com>
2021-10-04 23:50:09 -0300

    finish the application

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,42 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, Dispatch, SetStateAction } from  'react';
+import { Button } from "./Button";
+
+type SideBarProps = {
+  genres: Array<{
+    id: number;
+    name: any;
+    title: string;
+  }>
+  selectedGenreId: number;
+  setSelectedGenreId: Dispatch<SetStateAction<number>>;
+
+}
+
+export function SideBar({ 
+  genres, 
+  selectedGenreId, 
+  setSelectedGenreId 
+}: SideBarProps) {
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
